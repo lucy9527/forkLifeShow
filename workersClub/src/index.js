94858264b35205e4af784b9bc4fdf4258c5abb9c
lucy9527 <56916363+lucy9527@users.noreply.github.com>
2021-03-30 21:28:00 +0800

    Add files via upload

---
+++ workersClub/src/index.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './App';
+import reportWebVitals from './reportWebVitals';
+
+ReactDOM.render(
+  <React.StrictMode>
+    {/* 检查项目潜在问题的工具，严格模式仅在开发模式下运行，不影响生产构建 */}
+    <App />
+  </React.StrictMode>,
+  document.getElementById('root')
+);
+
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals();
